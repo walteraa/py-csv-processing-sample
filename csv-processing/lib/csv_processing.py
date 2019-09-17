f16a1d932d7dd4bf78b788db180c17a488fbef1d
Walter Alves <walter.arruda.alves@gmail.com>
2019-09-17 17:22:27 -0300

    Add csv_processing lib implementation

--- csv-processing/lib/csv_processing.py
+++ csv-processing/lib/csv_processing.py
@@ -1,3 +1,4 @@
+import csv, heapq
 class CSVParser:
     '''
         Class which represents the CSV parser
@@ -22,4 +23,33 @@ class CSVParser:
             Returns:
             list<dict>: The top n elements processed as a dictionary
         '''
-        raise Exception("Not implemented")
+
+        if self.file_path is None:
+            raise Exception('Invalid file')
+
+        if not categories:
+            raise Exception('Invalid categories')
+
+        if list_size < 1:
+            raise Exception('Invalid size')
+
+        if index < 0:
+            raise Exception('Invalid lambda index')
+
+        try:
+            with open(self.file_path, newline='') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+                data = map(lambda row: {'name': row[2],
+                                        'count': row[6],
+                                        'id': row[0],
+                                        'type': row[12]},
+                           heapq.nlargest(list_size,
+                                          filter(lambda e: e[12] in categories,
+                                                 reader
+                                                 ),
+                                          key=lambda e: int(e[index])
+                                          )
+                           )
+                return list(data)
+        except:
+            raise Exception('Invalid file')
