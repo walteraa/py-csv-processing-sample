f80c1684fa4d05566be9f21f0d2df1d6b3d49aa6
Walter Alves <walter.arruda.alves@gmail.com>
2019-09-17 15:25:04 -0300

    Fix top_n signature

--- csv-processing/lib/csv_processing.py
+++ csv-processing/lib/csv_processing.py
@@ -1,7 +1,16 @@
-
 class CSVParser:
+    '''
+        Class which represents the CSV parser
+    '''
+    def __init__(self, file_path):
+        '''
+            Constructor
+            Arguments:
+            file_path (str): The path of the file to be processed
+        '''
+        self.file_path = file_path
 
-    def top_n(list_size: int, index: int, *categories: str):
+    def top_n(self, list_size: int, index: int, *categories: str):
         ''' This method is used to get the top n entries of a csv considering the
             column index and the categories as filter
 
